1c6036e786fa8cba7c57d7ecfab5833f1ec9e53b
joe <joe94008@gmail.com>
2019-10-22 14:27:02 +0800

    ﻿<feat>:建立一個簡單的 express server
    連線到指定的 port 的時候回傳一個 hello world

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 
 const app = new express();
 
+app.get('/', (req, res) => {
+  res.write('hello world');
+  res.end();
+});
+
 app.listen(3000, () => {
   console.log('hello');
 });
